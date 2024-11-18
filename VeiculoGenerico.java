a46d3ca9587c68490a2a4f708304428c3b5baf76
Yan Araujo Resende <saresende555@gmail.com>
2024-11-18 16:48:34 -0300

    Segundo commit

--- VeiculoGenerico.java
+++ VeiculoGenerico.java
@@ -1,16 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Classe genérica
 public class VeiculoGenerico<T extends Carro> {
     private List<T> veiculos = new ArrayList<>();
 
-    // Adiciona um veículo à lista
     public void adicionarVeiculo(T veiculo) {
         veiculos.add(veiculo);
     }
 
-    // Retorna todos os veículos
     public List<T> getVeiculos() {
         return veiculos;
     }
